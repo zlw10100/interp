60796a7c1a32811caff76988ae29c0ecc29edecd
zlw10100 <330494152@qq.com>
2022-04-19 12:09:20 +0800

    优化导出。

--- struct.rkt
+++ struct.rkt
@@ -1,41 +1,21 @@
 #lang racket
 
+(provide 
+ (struct-out lookup-not-found)
+ (struct-out closure)
+ (struct-out native)
+ (struct-out builtin)
+ (struct-out continuation)
+ (struct-out pcontinuation)
+ (struct-out not-matched)
+ (struct-out not-bind )
+ )
+
 (struct lookup-not-found ())
-(provide lookup-not-found)
-(provide lookup-not-found?)
-
 (struct closure (params body env))
-(provide closure)
-(provide closure?)
-(provide closure-params)
-(provide closure-body)
-(provide closure-env)
-
 (struct native (procedure))
-(provide native)
-(provide native?)
-(provide native-procedure)
-
 (struct builtin (procedure))
-(provide builtin)
-(provide builtin?)
-(provide builtin-procedure)
-
 (struct continuation (procedure))
-(provide continuation)
-(provide continuation?)
-(provide continuation-procedure)
-
-; parial continuation
 (struct pcontinuation (procedure))
-(provide pcontinuation)
-(provide pcontinuation?)
-(provide pcontinuation-procedure)
-
 (struct not-matched ())
-(provide not-matched)
-(provide not-matched?)
-
 (struct not-bind ())
-(provide not-bind)
-(provide not-bind?)
